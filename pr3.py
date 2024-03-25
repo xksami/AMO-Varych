897f719d378468ad91bd5a7a1544e231fdfaa3d1
Anya Varich <137336129+xksami@users.noreply.github.com>
2024-03-25 16:50:12 +0200

    Add files via upload

---
+++ pr3.py
@@ -0,0 +1,72 @@
+def sort_bubble(arr):
+    n = len(arr)
+    for i in range(n):
+        swapped = False
+        for j in range(0, n-i-1):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+                swapped = True
+        if not swapped:
+            break
+    return arr
+
+def insert_sort(arr):
+    for i in range(1, len(arr)):
+        key = arr[i]
+        j = i - 1
+        while j >= 0 and key < arr[j]:
+            arr[j + 1] = arr[j]
+            j -= 1
+        arr[j + 1] = key
+    return arr
+
+a = [8, 10, 6, 2, 4]
+
+sorted_a = sort_bubble(a.copy())
+print("bubble Sort:")
+print("sort array:", sorted_a)
+
+sorted_a = insert_sort(a.copy())
+print("\ninsertion sort:")
+print("sorted array:", sorted_a)
+
+def merge_sort(arr):
+    if len(arr) <= 1:
+        return arr
+    mid = len(arr) // 2
+    left_half = merge_sort(arr[:mid])
+    right_half = merge_sort(arr[mid:])
+    return merge(left_half, right_half)
+
+def merge(left, right):
+    result = []
+    i = j = 0
+    while i < len(left) and j < len(right):
+        if left[i] < right[j]:
+            result.append(left[i])
+            i += 1
+        else:
+            result.append(right[j])
+            j += 1
+    result.extend(left[i:])
+    result.extend(right[j:])
+    return result
+
+arr = [12, 11, 13, 5, 6, 7]
+print("вхідний масив:", arr)
+sorted_arr = merge_sort(arr)
+print("відсортований масив:", sorted_arr)
+
+def quick_sort(arr):
+    if len(arr) <= 1:
+        return arr
+    pivot = arr[len(arr) // 2]
+    left = [x for x in arr if x < pivot]
+    middle = [x for x in arr if x == pivot]
+    right = [x for x in arr if x > pivot]
+    return quick_sort(left) + middle + quick_sort(right)
+
+arr = [12, 11, 13, 5, 6, 7]
+print("вхідний масив:", arr)
+sorted_arr = quick_sort(arr)
+print("відсортований масив:", sorted_arr)
